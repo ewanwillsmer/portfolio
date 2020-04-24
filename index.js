59d0fb5c4193864478e66c30417d2698269bff50
Ewan Willsmer <ewanwillsmer@gmail.com>
2020-04-24 15:01:37 +0200

    adding ScrollMagic, UL to card overlays and favicon

--- index.js
+++ index.js
@@ -4,8 +4,27 @@ $(document).ready(function () {
   $(".navbar").css("display", "inline-flex");
 });
 
-$(window).on("scroll", function () {
+$(document).on("scroll", function () {
   $(".navbar").css("background-color", "#333333").css("opacity", "60%");
 });
 
 // About me
+
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// build scene
+new ScrollMagic.Scene({
+  triggerElement: "#trigger1",
+  reverse: false, // only do once
+})
+  .setClassToggle("#reveal1", "visible")
+  .addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: "#trigger2",
+  reverse: false, // only do once
+})
+  .setClassToggle("line:before", "line:after")
+  .addIndicator()
+  .addTo(controller);
